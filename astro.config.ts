ebb1a4e56896cfea69703b98599b8ac8a14098d2
tomdam0 <thomas_damcevski@icloud.com>
2025-11-16 13:06:33 +1100

    changed adapter

--- astro.config.ts
+++ astro.config.ts
@@ -17,7 +17,7 @@ import path from 'path'
 import netlify from '@astrojs/netlify'
 
 export default defineConfig({
-  adapter: netlify(), // Set adapter for deployment, or set `linkCard` to `false` in `src/config.ts`
+  adapter: static(), // Set adapter for deployment, or set `linkCard` to `false` in `src/config.ts`
   site: themeConfig.site.website,
   image: {
     service: {
